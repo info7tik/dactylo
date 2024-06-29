9287b9126da7e2753cc91cccc86ccd558b452640
info7tik <info7tik@gmail.com>
2024-06-29 20:09:44 +0200

    countdown and statistics

---
+++ TODO.md
@@ -0,0 +1,5 @@
+* ajouter les informations
+  * vitesse moyenne de frappe débutant: 15 à 30 mots par minute
+  * vitesse moyenne de frappe expérimentée: 40 à 60 mots par minutes
+  * vitesse moyenne de frappe professionnelle: 80 à 100 mots par minutes
+  source: https://agilefingers.com/fr/objectif
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,63 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CountdownComponent } from './countdown/countdown.component';
+import { map, takeWhile, timer } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, CountdownComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-    title = 'dactylo';
-    beginText: string = "Les dinosaures forment un super-ordre ainsi qu'un clade extrêmement diversifié de sauropsides de la sous-classe des diapsides et dont les uniques représentants actuels sont les oiseaux. Ce sont des archosauriens ovipares, ayant en commun une posture érigée et partageant un certain nombre de synapomorphies telles que la présence   d'une crête deltopectorale allongée au niveau de l'humérus et un acetabulum perforant le bassi";
-    blueLetter = "n";
-    endText = ". Présentes";
-    countdownInSeconds = 30;
-    private isTimeoutRunning = false;
+    title = 'dactyl0';
+    beginText: string = "";
+    blueLetter = "L";
+    endText = "es dinosaures forment un super-ordre ainsi qu'un clade extrêmement diversifié de sauropsides de la sous-classe des diapsides et dont les uniques représentants actuels sont les oiseaux. Ce sont des archosauriens ovipares, ayant en commun une posture érigée et partageant un certain nombre de synapomorphies telles que la présence d'une crête deltopectorale allongée au niveau de l'humérus et un acetabulum perforant le bassin. Présentes";
+    instruction = "Start to type the text";
+    accuracyPercent = 0;
+    wordNumber = 0;
+    mistakeCounter = 0;
+    countdownInSeconds = 90;
+    remainingSeconds = this.countdownInSeconds;
+    private isRunningCountdown = false;
+    private isExpiredCountdown = false;
 
     @HostListener('document:keypress', ['$event'])
     handleKeyboardEvent(event: KeyboardEvent) {
-        if (!this.isTimeoutRunning) {
-            this.isTimeoutRunning = true;
-            setTimeout(this.decreaseTimeout, 1000);
+        if (!this.isRunningCountdown) {
+            this.isRunningCountdown = true;
+            const countdown = timer(0, 1000).pipe(map(n => this.countdownInSeconds - n), takeWhile(n => n > 0));
+            countdown.subscribe(() => {
+                this.remainingSeconds -= 1;
+                if (this.remainingSeconds == 0) {
+                    this.isExpiredCountdown = true;
+                    const properlyTypedWords: string[] = this.beginText.split(" ");
+                    if (properlyTypedWords.length == 0) {
+                        this.wordNumber = 0;
+                    } else {
+                        this.wordNumber = this.getLastElement(properlyTypedWords).length > 0 ?
+                            properlyTypedWords.length : properlyTypedWords.length - 1;
+                    }
+                    const properlyTypedCharacters = this.beginText.length;
+                    const totalHits = properlyTypedCharacters + this.mistakeCounter;
+                    this.accuracyPercent = properlyTypedCharacters / totalHits * 100;
+                }
+            });
         }
-        if (event.key == this.blueLetter) {
-            this.setNextCharInBlue();
-            if (this.endText.length == 0) {
-                this.endText = this.beginText.substring(1) + this.blueLetter
-                this.blueLetter = this.beginText[0]
-                this.beginText = ""
+        if (!this.isExpiredCountdown) {
+            if (event.key == this.blueLetter) {
+                this.setNextCharInBlue();
+                if (this.endText.length == 0) {
+                    this.endText = this.beginText.substring(1) + this.blueLetter;
+                    this.blueLetter = this.beginText[0];
+                    this.beginText = "";
+                }
+            } else {
+                this.mistakeCounter++;
             }
         } else {
-            console.log(event);
+            alert("Time is up!");
         }
     }
 
@@ -41,9 +67,7 @@ export class AppComponent {
         this.endText = this.endText.substring(1);
     }
 
-    decreaseTimeout() {
-        console.log("decreasing...");
-        this.countdownInSeconds = 10;
-        console.log(this.countdownInSeconds)
+    private getLastElement(array: any[]): any {
+        return array[array.length - 1];
     }
 }
