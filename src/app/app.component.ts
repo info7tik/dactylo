b32db008487fb6e56781c9967089ce5d30de5c1b
info7tik <info7tik@gmail.com>
2024-06-29 18:04:42 +0200

    enter the text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CountdownComponent } from './countdown/countdown.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, CountdownComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dactylo';
+    title = 'dactylo';
+    beginText: string = "Les dinosaures forment un super-ordre ainsi qu'un clade extrêmement diversifié de sauropsides de la sous-classe des diapsides et dont les uniques représentants actuels sont les oiseaux. Ce sont des archosauriens ovipares, ayant en commun une posture érigée et partageant un certain nombre de synapomorphies telles que la présence   d'une crête deltopectorale allongée au niveau de l'humérus et un acetabulum perforant le bassi";
+    blueLetter = "n";
+    endText = ". Présentes";
+    countdownInSeconds = 30;
+    private isTimeoutRunning = false;
+
+    @HostListener('document:keypress', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent) {
+        if (!this.isTimeoutRunning) {
+            this.isTimeoutRunning = true;
+            setTimeout(this.decreaseTimeout, 1000);
+        }
+        if (event.key == this.blueLetter) {
+            this.setNextCharInBlue();
+            if (this.endText.length == 0) {
+                this.endText = this.beginText.substring(1) + this.blueLetter
+                this.blueLetter = this.beginText[0]
+                this.beginText = ""
+            }
+        } else {
+            console.log(event);
+        }
+    }
+
+    setNextCharInBlue() {
+        this.beginText += this.blueLetter;
+        this.blueLetter = this.endText[0];
+        this.endText = this.endText.substring(1);
+    }
+
+    decreaseTimeout() {
+        console.log("decreasing...");
+        this.countdownInSeconds = 10;
+        console.log(this.countdownInSeconds)
+    }
 }
